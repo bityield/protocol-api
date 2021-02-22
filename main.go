c8a108e534253bb92f776446fb46a5650a5b11a2
Alex Manelis <amanelis@gmail.com>
2021-02-22 13:51:49 -0800

    Still redirecting

--- main.go
+++ main.go
@@ -45,24 +45,19 @@ func main() {
 		repeat = 5
 	}
 
-	secureMiddleware := secure.New(secure.Options{
-		SSLRedirect: true,
-	})
-
 	secureFunc := func() gin.HandlerFunc {
 		return func(c *gin.Context) {
-			err := secureMiddleware.Process(c.Writer, c.Request)
+			secureMiddleware := secure.New(secure.Options{
+				SSLRedirect: true,
+			})
 
 			// If there was an error, do not continue.
-			if err != nil {
-				c.Abort()
+			if err := secureMiddleware.Process(c.Writer, c.Request); err != nil {
+				log.Fatal(err)
 				return
 			}
 
-			// Avoid header rewrite if response is a redirection.
-			if status := c.Writer.Status(); status > 300 && status < 399 {
-				c.Abort()
-			}
+			c.Next()
 		}
 	}()
 
